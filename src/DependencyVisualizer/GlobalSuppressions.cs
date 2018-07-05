18f8eeb53db75427ecc328b9cb59f95391c40735
Laumania <mads@laumania.net>
2018-07-05 16:58:32 +0200

    Delete old version entirely
    Will rebuild from ground up, it's been 10 years after all :P

--- src/DependencyVisualizer/GlobalSuppressions.cs
+++
@@ -1,1 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification="This is an exe and Quickgraph isn't signed...")]
